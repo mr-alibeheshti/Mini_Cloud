64bb1f6a4c523da47c0f6a634ba960403e67d1ba
mr-alibeheshti <mr.developer.beheshti@gmail.com>
2024-10-07 15:23:19 +0330

    fix check Usage bash

--- check_usages.sh
+++ check_usages.sh
@@ -5,13 +5,11 @@ VG="minicloudVolume"
 THRESHOLD=80
 
 EMAIL="user@example.com"
-
+cd /dev/mapper
 LV_LIST=$(ls /dev/mapper/${VG}-*)
-
 for LV_PATH in $LV_LIST; do
   LV_NAME=$(basename $LV_PATH)
-  
-  USAGE=$(df -h | grep "$LV_PATH" | awk '{print $5}' | tr -d '%' | head -n 1)
+  USAGE=$(df -P $LV_PATH | awk 'NR==2 {print $5}' | tr -d '%')
   if  (( USAGE > THRESHOLD )) ; then
     MESSAGE="Warning: Volume $LV_NAME is $USAGE% full. Please take action."
     echo "$MESSAGE" | mail -s "LVM Usage Warning" "$EMAIL"
